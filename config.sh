25e24e3ad8ba3aba5b59786aa64912d9240b114d
Daniel Gheolţan <danielgheoltan@gmail.com>
2018-12-13 00:39:16 +0200

    Initialize

---
+++ config.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+PROJECT_PATH="D:/Magento/ce/v2.3.0"
+PROJECT_HOST="magento-ce-2-3-0.local"
+
+MYSQL_HOST="localhost"
+MYSQL_USERNAME="root"
+MYSQL_PASSWORD="root"
+MYSQL_DATABASE="magento-ce-2-3-0"
+
+MAGENTO_BASE_URL="http://magento-ce-2-3-0.local"
+MAGENTO_BACKEND_FRONTNAME="admin"
+MAGENTO_ADMIN_USER="admin"
+MAGENTO_ADMIN_PASSWORD="admin123"
+MAGENTO_ADMIN_FIRSTNAME="admin"
+MAGENTO_ADMIN_LASTNAME="admin"
+MAGENTO_ADMIN_EMAIL="noreply@domain.com"
---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+clear
+
+# -----------------------------------------------------------------------------
+
+source config.sh
+
+# -----------------------------------------------------------------------------
+
+./00-install.sh $1
+
+# call 10-install-__________-module-__________
+# call 11-install-__________-theme-__________
+# call 12-install-__________-languagepack-__________
+
+# call 20-install-__________-module-__________
+# call 21-install-__________-theme-__________
+# call 22-install-__________-languagepack-__________
+
+# -----------------------------------------------------------------------------
+
+### echo $PROJECT_PATH
+
+### call deploy
+
+# start "Front-end" /max "!MAGENTO_BASE_URL!"
+# start "Back-end" /max "!MAGENTO_BASE_URL!/!MAGENTO_BACKEND_FRONTNAME!"
